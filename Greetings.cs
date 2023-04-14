91b393a0df567a11a38920836e4dd5200d971cfa
beggsfinest00 <81796822+beggsfinest00@users.noreply.github.com>
2023-04-14 14:09:25 -0500

    Initial commit

---
+++ Greetings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classes_Dallas
+{
+    class Greetings
+    {
+        /// <summary>
+        /// prints Hello Traveler, welcome!
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+        /// <summary>
+        /// prints out name and thank you joining us today!
+        /// </summary>
+        /// <param name="name">prints out name</param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you joining us today!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace classes_Dallas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //links the Greetings.cs to this class.
+            Greetings greetings = new Greetings();
+            //prints out welcome line from greetings.
+            greetings.Welcome();
+
+            //ask for players name.
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+
+            //says hello to the player.
+            greetings.Hello(userName);
+        }
+    }
+}
